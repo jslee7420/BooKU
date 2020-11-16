89e24f27fb8386883395c42da17deee417c00ec8
jslee7420 <jeongsoo.lee7420@gmail.com>
2020-11-16 21:26:02 +0900

    UPDATE upload all files

---
+++ config/my_settings.py
@@ -0,0 +1,9 @@
+EMAIL = {
+'EMAIL_BACKEND' : 'django.core.mail.backends.smtp.EmailBackend',
+'EMAIL_USE_TLS' : True,
+'EMAIL_PORT' : 587,
+'EMAIL_HOST' : 'smtp.gmail.com',
+'EMAIL_HOST_USER' : 'jeongsoo.lee7420@gmail.com',                    
+'EMAIL_HOST_PASSWORD' : 'jssh0312',
+'REDIRECT_PAGE' : 'http://10.58.5.40:3000/signin'
+}
