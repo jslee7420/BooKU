4b966574b469347e2f0a89a1be7878e38fb026be
jslee7420 <jeongsoo.lee7420@gmail.com>
2021-01-15 09:38:48 +0900

    UPDATE date_of_birth default

--- user/models.py
+++ user/models.py
@@ -1,101 +1,102 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+import datetime
+
 
 class User(AbstractUser):
     GENDER_CHOICES = [
-        ('female','여'),
-        ('male','남'),
+        ('female', '여'),
+        ('male', '남'),
     ]
 
     MAJOR_CHOICES = [
-        ('국어국문학과','국어국문학과'),
-        ('영어영문학과','영어영문학과'),
-        ('중어중문학과','중어중문학과'),
-        ('철학과','철학과'),
-        ('사학과','사학과'),
-        ('지리학과','지리학과'),
-        ('미디어커뮤니케이션학과','미디어커뮤니케이션학과'),
-        ('문화컨텐츠학과','문화컨텐츠학과'),
-        ('수학과','수학과'),
-        ('물리학과','물리학과'),
-        ('화학과','화학과'),
-        ('건축학부','건축학부'),
-        ('사회환경공학부','사회환경공학부'),
-        ('기계항공학부','기계항공학부'),
-        ('전기전자공학부','전기전자공학부'),
-        ('화학공학부','화학공학부'),
-        ('컴퓨터공학부','컴퓨터공학부'),
-        ('산업경영공학부 산업공학과','산업경영공학부 산업공학과'),
-        ('산업경영공학부 신산업융합학과','산업경영공학부 신산업융합학과'),
-        ('생물공학과','생물공학과'),
-        ('k뷰티산업융합학과','k뷰티산업융합학과'),
-        ('정치외교학과','정치외교학과'),
-        ('경제학과','경제학과'),
-        ('행정학과','행정학과'),
-        ('국제무역학과','국제무역학과'),
-        ('응용통계학과','응용통계학과'),
-        ('융합인재학과','융합인재학과'),
-        ('글로벌비즈니스학과','글로벌비즈니스학과'),
-        ('경영학과','경영학과'),
-        ('기술경영학과','기술경영학과'),
-        ('부동산학과','부동산학과'),
-        ('미래에너지공학과','미래에너지공학과'),
-        ('스마트운행체공학과','스마트운행체공학과'),
-        ('스마트ICT융합공학과','스마트ICT융합공학과'),
-        ('화장품공학과','화장품공학과'),
-        ('줄기세포재생공학과','줄기세포재생공학과'),
-        ('의생명공학과','의생명공학과'),
-        ('시스템생명공학과','시스템생명공학과'),
-        ('융합생명공학과','융합생명공학과'),
-        ('생명과학특성학과','생명과학특성학과'),
-        ('동물자원과학과','동물자원과학과'),
-        ('식량자원공학과','식량자원공학과'),
-        ('축산식품생명공학과','축산식품생명공학과'),
-        ('식품유통공학과','식품유통공학과'),
-        ('환경보건과학과','환경보건과학과'),
-        ('산림조경학과','산림조경학과'),
-        ('수의예과','수의예과'),
-        ('수의학과','수의학과'),
-        ('커뮤니케이션디자인학과','커뮤니케이션디자인학과'),
-        ('산업디자인학과','산업디자인학과'),
-        ('의상디자인학과','의상디자인학과'),
-        ('리빙디자인학과','리빙디자인학과'),
-        ('현대미술학과','현대미술학과'),
-        ('영상영화학과','영상영화학과'),
-        ('일어교육과','일어교육과'),
-        ('수학교육과','수학교육과'),
-        ('체육교육과','체육교육과'),
-        ('음악교육과','음악교육과'),
-        ('교육공학과','교육공학과'),
-        ('영어교육과','영어교육과'),
-        ('교직과','교직과'),
+        ('국어국문학과', '국어국문학과'),
+        ('영어영문학과', '영어영문학과'),
+        ('중어중문학과', '중어중문학과'),
+        ('철학과', '철학과'),
+        ('사학과', '사학과'),
+        ('지리학과', '지리학과'),
+        ('미디어커뮤니케이션학과', '미디어커뮤니케이션학과'),
+        ('문화컨텐츠학과', '문화컨텐츠학과'),
+        ('수학과', '수학과'),
+        ('물리학과', '물리학과'),
+        ('화학과', '화학과'),
+        ('건축학부', '건축학부'),
+        ('사회환경공학부', '사회환경공학부'),
+        ('기계항공학부', '기계항공학부'),
+        ('전기전자공학부', '전기전자공학부'),
+        ('화학공학부', '화학공학부'),
+        ('컴퓨터공학부', '컴퓨터공학부'),
+        ('산업경영공학부 산업공학과', '산업경영공학부 산업공학과'),
+        ('산업경영공학부 신산업융합학과', '산업경영공학부 신산업융합학과'),
+        ('생물공학과', '생물공학과'),
+        ('k뷰티산업융합학과', 'k뷰티산업융합학과'),
+        ('정치외교학과', '정치외교학과'),
+        ('경제학과', '경제학과'),
+        ('행정학과', '행정학과'),
+        ('국제무역학과', '국제무역학과'),
+        ('응용통계학과', '응용통계학과'),
+        ('융합인재학과', '융합인재학과'),
+        ('글로벌비즈니스학과', '글로벌비즈니스학과'),
+        ('경영학과', '경영학과'),
+        ('기술경영학과', '기술경영학과'),
+        ('부동산학과', '부동산학과'),
+        ('미래에너지공학과', '미래에너지공학과'),
+        ('스마트운행체공학과', '스마트운행체공학과'),
+        ('스마트ICT융합공학과', '스마트ICT융합공학과'),
+        ('화장품공학과', '화장품공학과'),
+        ('줄기세포재생공학과', '줄기세포재생공학과'),
+        ('의생명공학과', '의생명공학과'),
+        ('시스템생명공학과', '시스템생명공학과'),
+        ('융합생명공학과', '융합생명공학과'),
+        ('생명과학특성학과', '생명과학특성학과'),
+        ('동물자원과학과', '동물자원과학과'),
+        ('식량자원공학과', '식량자원공학과'),
+        ('축산식품생명공학과', '축산식품생명공학과'),
+        ('식품유통공학과', '식품유통공학과'),
+        ('환경보건과학과', '환경보건과학과'),
+        ('산림조경학과', '산림조경학과'),
+        ('수의예과', '수의예과'),
+        ('수의학과', '수의학과'),
+        ('커뮤니케이션디자인학과', '커뮤니케이션디자인학과'),
+        ('산업디자인학과', '산업디자인학과'),
+        ('의상디자인학과', '의상디자인학과'),
+        ('리빙디자인학과', '리빙디자인학과'),
+        ('현대미술학과', '현대미술학과'),
+        ('영상영화학과', '영상영화학과'),
+        ('일어교육과', '일어교육과'),
+        ('수학교육과', '수학교육과'),
+        ('체육교육과', '체육교육과'),
+        ('음악교육과', '음악교육과'),
+        ('교육공학과', '교육공학과'),
+        ('영어교육과', '영어교육과'),
+        ('교직과', '교직과'),
     ]
 
+    university = models.CharField(max_length=20, default="Konkuk")
 
-    university=models.CharField(max_length=20, default="Konkuk")
-    
     first_major = models.CharField(
         max_length=30,
         choices=MAJOR_CHOICES,
         default=''
-        )
+    )
 
     second_major = models.CharField(
         max_length=30,
         choices=MAJOR_CHOICES,
         default='',
         blank=True,
-        )
+    )
 
     third_major = models.CharField(
         max_length=30,
         choices=MAJOR_CHOICES,
         default='',
         blank=True,
-        )
-    
+    )
+
     date_of_birth = models.DateField(
-        default='',
+        default=datetime.date.today,
     )
 
     gender = models.CharField(
@@ -109,49 +110,9 @@ class User(AbstractUser):
 
     def make_major_choices(self):
         major_choices = (
-            ('all','전체'),
-            ('major1',self.first_major),
-            ('major2',self.second_major),
+            ('all', '전체'),
+            ('major1', self.first_major),
+            ('major2', self.second_major),
             ('major3', self.third_major),
         )
         return major_choices
-
-    
-# class UserManager(BaseUserManager):    
-    
-#     use_in_migrations = True   
-    
-#     def create_user(self, email, username, password, first_major, second_major, third_major, date_of_birth, gender):        
-        
-#         if not email :            
-#             raise ValueError('must have user email')
-#         if not username:
-#             raise ValueError('must have username')
-#         user = self.model(            
-#             email = self.normalize_email(email),            
-#             username = username,
-#             first_major = first_major,
-#             second_major = second_major, 
-#             third_major = third_major,
-#             date_of_birth = date_of_birth,
-#             gender = gender,       
-#         )        
-#         user.set_password(password)        
-#         user.save(using=self._db)        
-#         return user     
-#     def create_superuser(self, email, nickname,password, first_major, second_major, third_major, date_of_birth, gender ):        
-       
-#         user = self.create_user(            
-#             email = self.normalize_email(email),            
-#             username = username,
-#             first_major = '',
-#             second_major = '', 
-#             third_major = '',
-#             date_of_birth = '',
-#             gender = '',             
-#         )        
-#         user.is_admin = True        
-#         user.is_superuser = True        
-#         user.is_staff = True        
-#         user.save(using=self._db)        
-#         return user 
