41eb626495f503d15e0ca1b17beb998d7849d7e5
jslee7420 <jeongsoo.lee7420@gmail.com>
2021-01-15 10:11:15 +0900

    STATIC_ROOT 생성, createsuperuser 변경

--- user/models.py
+++ user/models.py
@@ -3,6 +3,37 @@ from django.db import models
 import datetime
 
 
+class UserManager(BaseUserManager):
+    def _create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email mist be set')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, username, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff = True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser = True')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active = True')
+
+        return self._create_user(email, username, password, **extra_fields)
+
+
 class User(AbstractUser):
     GENDER_CHOICES = [
         ('female', '여'),
@@ -107,6 +138,8 @@ class User(AbstractUser):
 
     is_active = models.BooleanField(default=False)
     email = models.EmailField(unique=True, blank=False)
+
+    objects = UserManager()
 
     def make_major_choices(self):
         major_choices = (
