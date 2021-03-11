66bdd3b89b42a25d59b2844e74929e6b36cc2f60
jslee7420 <jeongsoo.lee7420@gmail.com>
2021-03-11 16:08:26 +0900

    back

--- config/settings/base.py
+++ config/settings/base.py
@@ -129,11 +129,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, "static", 'media'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "static", 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'user.User'
 
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,4 +2,5 @@ from .base import *
 
 ALLOWED_HOSTS = ['13.125.65.219', 'booku.club']
 STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
 DEBUG = False
