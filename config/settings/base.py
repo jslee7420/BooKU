ec86bfff7ec3186ac01bc5c36ac045eeabee8731
jslee7420 <jeongsoo.lee7420@gmail.com>
2021-03-11 16:30:43 +0900

    back

--- config/settings/base.py
+++ config/settings/base.py
@@ -127,12 +127,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'user.User'
 
