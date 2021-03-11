7904cf98c1a84fd35cf811f6181f44eea66833cf
jslee7420 <jeongsoo.lee7420@gmail.com>
2021-03-11 15:49:34 +0900

    UPDATE prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
 ALLOWED_HOSTS = ['13.125.65.219', 'booku.club']
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# STATIC_ROOT = BASE_DIR / 'static/'
+# STATICFILES_DIRS = []
 DEBUG = False
