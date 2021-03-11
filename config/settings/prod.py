9d136c017c765b1e2845dd9cc97ba5b4a6da74b3
jslee7420 <jeongsoo.lee7420@gmail.com>
2021-03-11 15:34:35 +0900

    UPDATE statictiles dirs

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,5 +2,5 @@ from .base import *
 
 ALLOWED_HOSTS = ['13.125.65.219', 'booku.club']
 STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 DEBUG = False
