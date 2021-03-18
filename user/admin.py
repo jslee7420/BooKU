a444874a4776eb92c630ccc5da9152f1174ecdb3
jslee7420 <jeongsoo.lee7420@gmail.com>
2021-03-18 17:33:28 +0900

    A

--- user/admin.py
+++ user/admin.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
-
 
 class CustomUserAdmin(UserAdmin):
-    UserAdmin.fieldsets[1][1]['fields'] += (
-        'first_major', 'second_major', 'third_major')
     UserAdmin.add_fieldsets += (
         (('Additional Info'), {
-         'fields': ('first_major', 'second_major', 'third_major')}),
+         'fields': ('first_major', 'second_major', 'third_major', 'email',  'is_active', 'is_staff',)}),
     )
+
+
+admin.site.register(User, UserAdmin)
