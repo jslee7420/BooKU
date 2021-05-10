0b3822f38c6130954faa4e67ea519f23a9852b16
jslee7420 <46511190+jslee7420@users.noreply.github.com>
2021-05-10 10:36:58 +0900

    Delete my_settings.py

--- config/my_settings.py
+++
@@ -1,9 +0,0 @@
-EMAIL = {
-'EMAIL_BACKEND' : 'django.core.mail.backends.smtp.EmailBackend',
-'EMAIL_USE_TLS' : True,
-'EMAIL_PORT' : 587,
-'EMAIL_HOST' : 'smtp.gmail.com',
-'EMAIL_HOST_USER' : 'booku2021@gmail.com',                    
-'EMAIL_HOST_PASSWORD' : 'bookonkuk2021',
-'REDIRECT_PAGE' : 'http://10.58.5.40:3000/signin'
-}
